635eefc57c81c3b5a1f3481d9f5f98d3510df06b
SandersBrandon <brandosan1@icloud.com>
2021-09-19 12:40:33 -0400

    adding solution 10

---
+++ exercise10/src/main/java/exercise10/Solution10.java
@@ -0,0 +1,50 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Brandon Sanders
+ */
+package exercise10;
+
+import java.util.Scanner;
+
+public class Solution10 {
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main (String[] args){
+        System.out.println("Enter the price of item 1: ");
+        String itemOne = in.nextLine();
+        System.out.println("Enter the quantity of item 1: ");
+        String itemOneQuantity = in.nextLine();
+        System.out.println("Enter the price of item 2: ");
+        String itemTwo = in.nextLine();
+        System.out.println("Enter the quantity of item 2: ");
+        String itemTwoQuantity = in.nextLine();
+        System.out.println("Enter the price of item 3: ");
+        String itemThree = in.nextLine();
+        System.out.println("Enter the quantity of item 3: ");
+        String itemThreeQuantity = in.nextLine();
+
+        double taxRate = 0.055;
+
+        double priceOne = Integer.parseInt(itemOne);
+        double quantityOne = Integer.parseInt(itemOneQuantity);
+        double priceTwo = Integer.parseInt(itemTwo);
+        double quantityTwo = Integer.parseInt(itemTwoQuantity);
+        double priceThree = Integer.parseInt(itemThree);
+        double quantityThree = Integer.parseInt(itemThreeQuantity);
+
+        double subtotalPrice = (priceOne * quantityOne) + (priceTwo * quantityTwo) + (priceThree * quantityThree);
+
+        double taxPrice = taxRate * subtotalPrice;
+
+        double totalPrice = taxPrice + subtotalPrice;
+
+        System.out.println("Subtotal: $" + String.format("%.2f", subtotalPrice));
+        System.out.println("Tax: $"+ String.format("%.2f", taxPrice) );
+        System.out.println("Total: $" + String.format("%.2f", totalPrice) );
+
+
+
+    }
+
+}
