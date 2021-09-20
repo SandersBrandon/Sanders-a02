e99a96e956a760edd43f775a64f755bab781e613
SandersBrandon <brandosan1@icloud.com>
2021-09-19 22:31:26 -0400

    adding solution 22

--- exercise22/src/main/java/exercise22/Solution22.java
+++ exercise22/src/main/java/exercise22/Solution22.java
@@ -6,10 +6,44 @@ package exercise22;
 
 import java.util.Scanner;
 
+/* Pseudocode
+*Ask for user input obtain input convert into numerical data
+* Check to see if any are the same number if yes exit the program
+* If they all different check to see which is the largest number
+* Output statement with require information
+*/
+
 public class Solution22 {
     private static final Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        System.out.println("Enter the first number:");
+        String firstAns = in.nextLine();
+        System.out.println("Enter the second number:");
+        String secondAns = in.nextLine();
+        System.out.println("Enter the third number:");
+        String thirdAns = in.nextLine();
+
+        int firstNum = Integer.parseInt(firstAns);
+        int secondNum = Integer.parseInt(secondAns);
+        int thirdNum = Integer.parseInt(thirdAns);
+
+        if(firstNum == secondNum || firstNum ==thirdNum || secondNum == thirdNum){
+            System.exit(0);
+        }
+
+            if(firstNum > secondNum && firstNum >thirdNum){
+
+            System.out.println("The largest number is "+ firstNum);
+            }
+
+            if(secondNum > firstNum && secondNum >thirdNum){
+
+            System.out.println("The largest number is "+ secondNum);
+        }
+             if(thirdNum > secondNum && thirdNum >firstNum){
+
+            System.out.println("The largest number is "+ thirdNum);
+        }
     }
 }
